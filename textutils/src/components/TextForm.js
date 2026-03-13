e2563b30550acf5abe87bac547170de74529fcfe
Abhay Verma <abhayoct04@gmail.com>
2026-03-13 16:07:43 +0530

    Added Convert to lowercase button, text summary and preview

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -8,14 +8,28 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
-    const [text, setText] = useState('Enter Text Here!!!')
+    const handleLowClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const [text, setText] = useState('')
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <>
+            <div className='container'>
+                <h2>{props.heading}</h2>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{text.split(" ").length * 0.008} Minutes to read the text</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
