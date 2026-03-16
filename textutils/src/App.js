528408111499f0e3eb532198dd161f18b14625af
Abhay Verma <abhayoct04@gmail.com>
2026-03-16 19:51:19 +0530

    Added React Routing

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,42 +2,62 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
-  const showAlert = (message, type)=>{
+
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
-    })
-    setTimeout(()=>{
+    });
+
+    setTimeout(() => {
       setAlert(null);
-    }, 1500)
-  }
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark Mode is enabled", "success")
-    }
-    else{
+      showAlert("Dark Mode is enabled", "success");
+    } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light Mode is enabled", "success")
+      showAlert("Light Mode is enabled", "success");
     }
-  }
+  };
+
   return (
-    <>
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
+
       <div className="container my-3">
-        <TextForm heading = "Enter the text to analyze" showAlert={showAlert} mode={mode} />
-        <About mode={mode} />
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode} />} />
+          <Route
+            exact path="/"
+            element={
+              <TextForm
+                heading="Enter the text to analyze"
+                showAlert={showAlert}
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
